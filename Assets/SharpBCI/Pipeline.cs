9eb1c8e5743de3b7240695b95051d7f2a39d5365
Jon Monroe <berkona@gmail.com>
2017-09-18 02:55:02 -0400

    Adding pipeline and upped C# compat to 4.6 & unity 2017

---
+++ Assets/SharpBCI/Pipeline.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.Collections.Concurrent;
+
+namespace SharpBCI {
+
+	public interface IPipeable<In, Out> {
+		/*
+		 * Set the input on this IPipeable to param input
+		 */
+		void SetInput(BlockingCollection<In> input);
+
+		/*
+		 * Connect the input of other to our output
+		 */
+		void Connect(IPipeable<Out, object> other);
+
+		/*
+		 * Actually start doing work (i.e., promise to eventually start pushing data to connected pipeables)
+		 */
+		void Start(TaskFactory taskFactory, CancellationTokenSource cts);
+
+		/*
+		 * Require this IPipeable to stop, blocking until actually stopped
+		 */
+		void Stop();
+	}
+
+	public abstract class Pipeable<In, Out> : IPipeable<In, Out> {
+		Task runningTask;
+		CancellationTokenSource cts;
+		CancellationToken token;
+
+		BlockingCollection<In> input;
+
+		public static explicit operator Pipeable<In, Out>(EEGDeviceProducer v)
+		{
+			throw new NotImplementedException();
+		}
+
+		// TODO do we need to limit the size of this buffer due to memory concerns?
+		BlockingCollection<Out> output = new BlockingCollection<Out>();
+
+		public void SetInput(BlockingCollection<In> input) {
+			this.input = input;
+		}
+
+		public void Connect(IPipeable<Out, object> other) {
+			other.SetInput(output);		
+		}
+
+		public virtual void Start(TaskFactory taskFactory, CancellationTokenSource cts) {
+			this.cts = cts;
+			this.token = cts.Token;
+			runningTask = taskFactory.StartNew(Run);
+		}
+
+		public virtual void Stop() {
+			// TODO do cooperative stopping here, or above us?
+			runningTask.Wait();
+		}
+
+		void Run() {
+			try {
+				// case: producer
+				if (input == null) {
+					do {
+						if (token.IsCancellationRequested) break;
+					} while (Process(default(In)));
+				}
+				// case: consumer (possibly a filter)
+				else {
+					foreach (var item in input.GetConsumingEnumerable(token)) {
+						if (token.IsCancellationRequested || !Process(item))
+							break;
+					}
+				}
+			} catch (Exception e) {
+				cts.Cancel();
+				if (!(e is OperationCanceledException))
+					throw;
+			} finally {
+				output.CompleteAdding();
+			}
+		}
+
+		protected void Add(Out item) {
+			output.Add(item, token);
+		}
+
+		protected abstract bool Process(In item);
+	}
+}
---
+++ Assets/SharpBCI/SharpBCI.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharpBCI {
+
+	public class SharpBCIConfig {
+		public EEGDeviceAdapter adapter;
+		public int channels;
+	}
+
+	public class SharpBCI {
+		readonly EEGDeviceAdapter adapter;
+		readonly int channels;
+
+		List<Pipeable<object, object>> stages;
+
+		public SharpBCI(SharpBCIConfig config) {
+			// begin check args
+			if (config.adapter == null)
+				throw new ArgumentException("config.adapter must not be null");
+
+			if (channels <= 0)
+				throw new ArgumentException("config.channels must be > 0");
+			// end check args
+
+			// begin state config
+			adapter = config.adapter;
+			channels = config.channels;
+			// end state config
+
+			// begin internal pipeline construction
+			var producer = new EEGDeviceProducer(adapter);
+			stages.Add((Pipeable<object, object>) producer);
+
+			// TODO other stages
+
+			// end internal pipeline construction
+
+			// begin start associated threads & EEGDeviceAdapter
+			TaskFactory f = new TaskFactory(TaskCreationOptions.LongRunning, TaskContinuationOptions.None);
+			CancellationTokenSource cts = CancellationTokenSource.CreateLinkedTokenSource();
+			producer.Start(f, cts);
+
+			// TODO other stages
+
+			// end start associated threads & EEGDeviceAdapter
+		}
+
+		public void Close() {
+			foreach (var stage in stages) {
+				stage.Stop();
+			}
+		}
+	}
+
+	public class EEGDeviceProducer : Pipeable<object, EEGEvent> {
+		readonly EEGDeviceAdapter adapter;
+		public EEGDeviceProducer(EEGDeviceAdapter adapter) {
+			this.adapter = adapter;
+		}
+
+		public override void Start(TaskFactory taskFactory, CancellationTokenSource cts) {
+			adapter.AddHandler(EEGDataType.EEG, Add);
+			adapter.Start();
+			base.Start(taskFactory, cts);
+		}
+
+		public override void Stop() {
+			adapter.Stop();
+			base.Stop();
+		}
+
+		protected override bool Process(object item) {
+			adapter.FlushEvents();
+			return true;
+		}
+	}
+}
