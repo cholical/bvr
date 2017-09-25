e9063e8fdde12dba3c280187f18c4f7cb6648325
Jon Monroe <berkona@gmail.com>
2017-09-25 18:46:39 -0400

    Prefabs & particle test
    Added prefab for debug UI;
    Added prefab for brain;
    Updates to ParticleBrainWaveController;
    Fix ScrollingGraphController;

--- Assets/ParticleBrainWaveController.cs
+++ Assets/ParticleBrainWaveController.cs
@@ -5,6 +5,14 @@ using SharpBCI;
 
 public class ParticleBrainWaveController : MonoBehaviour {
 
+	static Dictionary<EEGDataType, Color> colorsByType = new Dictionary<EEGDataType, Color> { 
+		{ EEGDataType.ALPHA_RELATIVE, new Color32(255, 0, 0, 200) },
+		{ EEGDataType.BETA_RELATIVE, new Color32(0, 0, 255, 200) },
+		{ EEGDataType.GAMMA_RELATIVE, new Color32(0, 230, 0, 200) },
+		{ EEGDataType.DELTA_RELATIVE, new Color32(255, 0, 227, 200) },
+		{ EEGDataType.THETA_RELATIVE, new Color32(0, 255, 255, 200) },
+	};
+
 	public int channel;
 	public EEGDataType dataType;
 	public double particleFactor = 10;
@@ -15,6 +23,9 @@ public class ParticleBrainWaveController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		system = GetComponent<ParticleSystem>();
+		var main = system.main;
+		main.startColor = colorsByType[dataType];
 		SharpBCIController.BCI.AddRawHandler(dataType, OnEEGData);
 	}
 
