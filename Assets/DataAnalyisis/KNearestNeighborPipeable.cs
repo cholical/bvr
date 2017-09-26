40d7a4bf4be42fcc396dd28d6eda0a86b1e75505
Mark Molinaro <markjm@live.unc.edu>
2017-09-26 19:17:25 -0400

    issue

--- Assets/DataAnalyisis/KNearestNeighborPipeable.cs
+++ Assets/DataAnalyisis/KNearestNeighborPipeable.cs
@@ -11,22 +11,6 @@ namespace SharpBCI
 		private List<double> buffer;
 		private int training;
 
-
-<<<<<<< HEAD
-		public KNearestNeighborPipeable(int bufferSize) {
-			this.knn = new KNearestNeighbor (1);
-			this.bufferSize = bufferSize;
-			buffer = new List<double>();
-			training = 0;
-
-		}
-
-		public void StartTraining(int id) {
-			training = id;
-		}
-
-		public void StopTraining(int id) {
-=======
 		public KNearestNeighborPipeable (int bufferSize)
 		{
 			this.knn = new KNearestNeighbor (1);
@@ -44,7 +28,6 @@ namespace SharpBCI
 
 		public void StopTraining (int id)
 		{
->>>>>>> master
 			training = 0;
 		}
 
@@ -53,21 +36,8 @@ namespace SharpBCI
 			if (!(item is double)) {
 				return false;
 			}
-
-<<<<<<< HEAD
+				
 			buffer.Add ((double) item);
-
-			if (buffer.Count == bufferSize) {
-				lock(training) {
-					if(training != 0) {
-						AddTrainingData(training, buffer.ToArray());
-					}else {
-						Add (knn.Predict (buffer.ToArray ()));
-					}
-			}
-			}
-=======
-			buffer.Add ((double)item);
 
 			if (buffer.Count == bufferSize) {
 				if (training != 0) {
@@ -76,17 +46,12 @@ namespace SharpBCI
 					Add (new TrainedEvent (knn.Predict (buffer.ToArray ())));
 				}
 				buffer.Clear();
->>>>>>> master
 			}
 			return true;
 		}
-
-<<<<<<< HEAD
-		private void AddTrainingData(int label, double[] data) {
-=======
+			
 		private void AddTrainingData (int label, double[] data)
 		{
->>>>>>> master
 			knn.AddTrainingData (label, data);
 		}
 			
