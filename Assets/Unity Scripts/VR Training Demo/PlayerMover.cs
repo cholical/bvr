813fc3cdbdef0f845c61b73acc3e482655aa39b7
Jon Monroe <berkona@gmail.com>
2017-10-06 16:24:38 -0400

    Setup for Flappy Bird
    Added grass textures for flappy-bird;
    Added a terrain object for flappy-bird;
    Added terrain movement script to simulate forward movement;

---
+++ Assets/Unity Scripts/VR Training Demo/PlayerMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+	public float forwardSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * forwardSpeed * Time.deltaTime;	
+	}
+}
---
+++ Assets/Unity Scripts/VR Training Demo/TerrainSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSpawner : MonoBehaviour {
+
+	public GameObject terrainPrefab;
+	public int nSpawn = 5;
+	public float moveSpeed = 1;
+
+	Vector3 terrainSize;
+
+	readonly LinkedList<GameObject> terrainList = new LinkedList<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		//offset = transform.position;
+		terrainSize = terrainPrefab.GetComponent<Terrain>().terrainData.size;
+		for (int i = -nSpawn; i < nSpawn; i++) {
+			var pos = Vector3.forward * i * terrainSize.z;
+			var t = Instantiate(terrainPrefab, pos, Quaternion.Euler(Vector3.zero));
+			terrainList.AddLast(t);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (var terrain in terrainList) {
+			terrain.transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+		}
+		var backTerrain = terrainList.First.Value;
+		var distFromBack = Mathf.Abs(backTerrain.transform.position.z);
+		if (distFromBack > nSpawn * terrainSize.z) {
+			terrainList.RemoveFirst();
+			Destroy(backTerrain);
+			var forwardTerrain = terrainList.Last.Value;
+			var newPos = forwardTerrain.transform.position + Vector3.forward * terrainSize.z;
+			var t = Instantiate(terrainPrefab, newPos, Quaternion.Euler(Vector3.zero));
+			terrainList.AddLast(t);
+		}
+	}
+}
