2243a985501752d2cc79749cce5337d373f48185
Grady Hale <grhale@live.unc.edu>
2017-11-07 22:52:48 -0500

    newest-coins

---
+++ Assets/Unity Scripts/Flappy Bird Demo/CameraCoinCollide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCoinCollide : MonoBehaviour {
+	public static int coinScore;
+
+	public CameraCoinCollide(){
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		print ("Collision");
+		coinScore++;
+		Destroy(collision.gameObject);
+	}
+		
+}
---
+++ Assets/Unity Scripts/Flappy Bird Demo/CoinManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinManager : MonoBehaviour {
+	public Text scoreLabel;
+	void Start()
+	{
+		scoreLabel.text = "0";
+	}
+
+	void Update()
+	{
+		scoreLabel.text = CameraCoinCollide.coinScore.ToString();
+	}
+
+}
