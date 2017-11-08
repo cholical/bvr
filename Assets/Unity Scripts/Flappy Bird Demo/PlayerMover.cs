0e45057d7fc26caf1cb214759477cff20f8a10af
Jon Monroe <berkona@gmail.com>
2017-11-08 13:35:52 -0500

    Lots of graphics tweaks

--- Assets/Unity Scripts/Flappy Bird Demo/PlayerMover.cs
+++ Assets/Unity Scripts/Flappy Bird Demo/PlayerMover.cs
@@ -6,6 +6,8 @@ public class PlayerMover : MonoBehaviour {
 
 	public float movementRange = 1.75f;
 	public float timeFactor = 7.5f;
+	public float forwardSpeed = 5f;
+	public float teleportDist = 50f;
 
 	Vector3 originalPos;
 
@@ -15,6 +17,14 @@ public class PlayerMover : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		transform.position = originalPos + Vector3.up * movementRange * Mathf.Sin (2 * Mathf.PI * (Time.time / timeFactor));
+		//if (!useSharpBCI) {
+		//	transform.position = originalPos + Vector3.up * movementRange * Mathf.Sin(2 * Mathf.PI * (Time.time / timeFactor));
+		//}
+		transform.position += Vector3.forward * forwardSpeed * Time.deltaTime;
+		if (Mathf.Abs(transform.position.z - originalPos.z) >= teleportDist) {
+			var pos = transform.position;
+			pos.z -= teleportDist;
+			transform.position = pos;
+		}
 	}
 }
