8fbda301e0919b38c009306dfe21bb1fb8913b09
Jonathan William Monroe <jwmonroe@cs.unc.edu>
2017-11-28 19:25:49 -0500

    Syntax fix

--- Assets/SharpBCIController.cs
+++ Assets/SharpBCIController.cs
@@ -151,7 +151,7 @@ public class SharpBCIController : MonoBehaviour {
 			.EEGDeviceAdapter(adapter)
 			.PipelineFile(System.IO.Path.Combine(Application.streamingAssetsPath, "default_pipeline.json"))
 			.Build();
-		BCI.logRawData(dataType);
+		BCI.LogRawData(dataType);
 	}
 
 	void OnDestroy() {
