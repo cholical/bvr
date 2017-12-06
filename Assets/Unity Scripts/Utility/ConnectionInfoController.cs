5569f96c0acf5cbc5027bb36fa4eed6ef12d9c28
Jon Monroe <berkona@gmail.com>
2017-12-05 19:06:27 -0500

    Added new connection status UI
    Also added scene for testing (Scenes/connection-testing)

---
+++ Assets/Unity Scripts/Utility/ConnectionInfoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class ConnectionInfoController : MonoBehaviour {
+
+	public ConnectionInfoDisplay leftTemple;
+	public ConnectionInfoDisplay leftForehead;
+	public ConnectionInfoDisplay rightForehead;
+	public ConnectionInfoDisplay rightTemple;
+
+	public bool showStatus { 
+		get { return _showStatus; }
+		set {
+			if (value != _showStatus) {
+				_showStatus = value;
+				leftTemple.gameObject.SetActive(value);
+				leftForehead.gameObject.SetActive(value);
+				rightForehead.gameObject.SetActive(value);
+				rightTemple.gameObject.SetActive(value);
+			}
+			// noop otherwise
+		}
+	}
+
+	bool _showStatus = false;
+	double _lastStatus = 0.0;
+
+	// Update is called once per frame
+	void Update () {
+		var status = SharpBCIController.BCI.connectionStatus;
+		leftTemple.AddContactQuality((int)status[0]);
+		leftForehead.AddContactQuality((int)status[1]);
+		rightForehead.AddContactQuality((int)status[2]);
+		rightTemple.AddContactQuality((int)status[3]);
+
+		var nextStatus = status.Sum();
+		if (!showStatus && Math.Abs(_lastStatus - nextStatus) > 0.1) {
+			showStatus = true;
+			nextStatus = _lastStatus;
+		}
+	}
+}
---
+++ Assets/Unity Scripts/Utility/ConnectionInfoDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ConnectionInfoDisplay : MonoBehaviour {
+
+	public Color badColor = Color.red;
+	public Color goodColor = Color.green;
+	public int averageSize = 100;
+
+	Image _image;
+	Queue<int> _samples = new Queue<int>();
+	float _currentQuality = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		_image = GetComponent<Image>();
+		_currentQuality = 0.0f;
+	}
+
+	public void AddContactQuality(int quality) {
+		_currentQuality += quality / ((float)averageSize);
+		_samples.Enqueue(quality);
+		if (_samples.Count >= averageSize) {
+			_currentQuality -= _samples.Dequeue() / ((float)averageSize);
+		}
+		// transform from [1, 4] to [0, 1]
+		float lerped = (_currentQuality - 1.0f) / 3.0f;
+		_image.color = Color.Lerp(goodColor, badColor, lerped);
+	}
+}
